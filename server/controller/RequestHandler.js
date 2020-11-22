a5414538365c805c98c4508f6f74cb86b7012294
Tedros <tedtfu@gmail.com>
2020-11-22 17:01:46 -0500

    server

--- server/controller/RequestHandler.js
+++ server/controller/RequestHandler.js
@@ -4,7 +4,7 @@ const {router}=require('./RouteHandler');
 
 const app=express();
 const cors=require('cors');
-const PORT=5000;
+const PORT=process.env.PORT||5000;
 
 app.listen(PORT,()=>{
     console.log(`server started listening on ${PORT}`)
